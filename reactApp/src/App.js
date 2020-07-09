ba94b6496e041226e28b374dff62e22699dcaee6
huxinyu <huxinyu.akiha@bytedance.com>
2020-07-09 15:22:46 +0800

    remove useless files
    Change-Id: Ie27811b21ed65fbee86c168fed3103c8bc671336

--- reactApp/src/App.js
+++ reactApp/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchPage from './pages/SearchPage';
 import ResultPage from './pages/ResultPage';
+import { GameInfo } from './components/GameInfo'
 
 function App() {
   return (
     <Router>
       <Route path="/home" component={SearchPage} />
       <Route path="/result" component={ResultPage} />
+      <Route path="/info" component={GameInfo} />
     </Router>
   );
 }
