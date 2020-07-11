b72644a786364cb4ee2c1669d7eeadcadeed811e
fangtaoran <fangtaoran@bytedance.com>
2020-07-11 16:46:45 +0800

    master:modify the word map.

--- goServer/pkg/Server/wordMap.go
+++ goServer/pkg/Server/wordMap.go
@@ -9,7 +9,19 @@ func WordListInit() error {
 	WordList["美国末日"] = "最后的生还者"
 	WordList["gta"] = "侠盗猎车"
 	WordList["老滚"] = "上古卷轴"
-
+	WordList["lol"] = "英雄联盟"
+	WordList["刀塔"] = "dota"
+	WordList["lol"] = "英雄联盟"
+	WordList["ow"] = "守望先锋"
+	WordList["dnf"] = "地下城与勇士"
+	WordList["农药"] = "王者荣耀"
+	WordList["吃鸡"] = "绝地求生"
+	WordList["黑魂"] = "黑暗之魂"
+	WordList["骑砍"] = "骑马与砍杀"
+	WordList["怪猎"] = "怪物猎人"
+	WordList["大表哥"] = "荒野大镖客"
+	WordList["神海"] = "神秘海域"
+	WordList["动森"] = "动物森友会"
 	return nil
 }
 
